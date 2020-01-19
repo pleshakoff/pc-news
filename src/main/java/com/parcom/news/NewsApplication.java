9850442b736b4b1ab69fd6e9b0b150b5baf754ec
pleshakoff <pleshakoff@gmail.com>
2020-01-20 00:52:27 +0300

    Подключил сброс метрик в prometheus

--- src/main/java/com/parcom/news/NewsApplication.java
+++ src/main/java/com/parcom/news/NewsApplication.java
@@ -3,9 +3,14 @@ package com.parcom.news;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 @EnableAsync
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class NewsApplication {
 
 
@@ -14,6 +19,7 @@ public class NewsApplication {
 //  docker image push pleshakoff/pc-news:hw1
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
 
